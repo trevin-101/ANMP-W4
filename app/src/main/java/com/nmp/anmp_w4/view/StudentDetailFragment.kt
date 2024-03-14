26cfc0c220736394fb53ee0139bad946a8e5dc8b
trevin <trevin.juanli@gmail.com>
2024-03-14 16:43:03 +0700

    added swipefreshlayout

---
+++ app/src/main/java/com/nmp/anmp_w4/view/StudentDetailFragment.kt
@@ -0,0 +1,21 @@
+package com.nmp.anmp_w4.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.nmp.anmp_w4.R
+
+
+class StudentDetailFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_student_detail, container, false)
+    }
+
+}
---
+++ app/src/main/java/com/nmp/anmp_w4/view/StudentListFragment.kt
@@ -0,0 +1,22 @@
+package com.nmp.anmp_w4.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.nmp.anmp_w4.R
+
+
+class StudentListFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_student_list, container, false)
+    }
+
+}
