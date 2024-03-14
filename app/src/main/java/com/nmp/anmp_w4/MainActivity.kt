d66f5447124be7d0e3e3ce6ce10dc8023b44785d
trevin <trevin.juanli@gmail.com>
2024-03-14 16:18:22 +0700

    added builldscript and MainActivity

---
+++ app/src/main/java/com/nmp/anmp_w4/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nmp.anmp_w4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,12 @@
+buildscript {
+    repositories {
+        google()
+    }
+    dependencies {
+        classpath ("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.7")
+    }
+}
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.1.1" apply false
